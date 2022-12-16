dabc2db61997a3b7189d2ba12206f34022ee20ed
prasunmatrix <prasun@matrixnmedia.com>
2022-12-16 17:56:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+//import Mycomponent from './components/Greet';
+import { Greet } from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import  Greet1  from './components/Greet1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
         <a
           className="App-link"
@@ -17,7 +22,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <Mycomponent/> */}
+      <Greet/>
+      <Greet1 name="Bruce"><p>This is children props</p></Greet1>
+      <Greet1 name="Clark" />
+      <Welcome/>
+      <Hello/>
     </div>
   );
 }
---
+++ src/components/Greet1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function Greet1() {
+//   return <h1>Hello Vishwas New</h1>
+// }
+
+const Greet1 =props =>{
+  //console.log(props)
+  return <h1>Hello {props.name}</h1>
+}
+//const Greet1 =() =><h1>Hello Vishwas New</h1>
+
+
+
+ export default Greet1 
